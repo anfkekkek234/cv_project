7d34df9ca2613979bbf6c329f425ba73f93199ee
Ali <awp.828.cr7@email.com>
2024-05-12 11:56:46 +0330

    edit
    edit render

--- home/views.py
+++ home/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{'name':'Alireza Konarizadeh','age':'23','gender':'Man',
+                                        'languages':'English and Persian',
+                                        'degree': 'Student',
+                                        'email': 'awp.828.cr7@gmail.com',
+                                        'city':'Bandar Abbas, Hormozgan, Iran',
+                                        'freelance':'Available'})
